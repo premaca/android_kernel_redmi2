a6ce4706c81ee33eeba272bf1630dec922a2bf3a
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-15 11:58:27 +0700

    media: radio: IRIS transport without firmware loading support
    This reverts commit d5d677e0be61b918307e1dfe331daecb1eb395ba.
    
    Conflicts:
    	drivers/media/radio/radio-iris-transport.c
    
    Change-Id: If93d31bd69f12fe9a89c037c2a450892f695fb48
    
    Conflicts:
    	drivers/media/radio/radio-iris-transport.c
    	drivers/media/radio/radio-iris.c
    
    Conflicts:
    	drivers/media/radio/radio-iris-transport.c
    	drivers/media/radio/radio-iris.c

--- drivers/media/radio/radio-iris-transport.c
+++ drivers/media/radio/radio-iris-transport.c
@@ -38,14 +38,20 @@ struct radio_data {
 	struct smd_channel  *fm_channel;
 };
 struct radio_data hs;
-DEFINE_MUTEX(fm_smd_enable);
-static int fmsmd_set;
-static bool chan_opened;
+
+#ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
+static DEFINE_MUTEX(fm_smd_enable);
+static int fmsmd_set = 0;
 static int hcismd_fm_set_enable(const char *val, struct kernel_param *kp);
 module_param_call(fmsmd_set, hcismd_fm_set_enable, NULL, &fmsmd_set, 0644);
-static struct work_struct *reset_worker;
 static void radio_hci_smd_deregister(void);
 static void radio_hci_smd_exit(void);
+#else
+int fmsmd_ready = -1;
+void radio_hci_smd_deregister(void);
+#endif
+static bool chan_opened;
+static struct work_struct *reset_worker;
 
 static void radio_hci_smd_destruct(struct radio_hci_dev *hdev)
 {
@@ -174,7 +180,9 @@ static int radio_hci_smd_register_dev(struct radio_data *hsmd)
 		(unsigned long) hsmd);
 	hdev->send  = radio_hci_smd_send_frame;
 	hdev->destruct = radio_hci_smd_destruct;
+#ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 	hdev->close_smd = radio_hci_smd_exit;
+#endif
 
 	/* Open the SMD Channel and device and register the callback function */
 	rc = smd_named_open_on_edge("APPS_FM", SMD_APPS_WCNSS,
@@ -201,7 +209,11 @@ static int radio_hci_smd_register_dev(struct radio_data *hsmd)
 	return 0;
 }
 
+#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
+void radio_hci_smd_deregister(void)
+#else
 static void radio_hci_smd_deregister(void)
+#endif
 {
 	radio_hci_unregister_dev();
 	kfree(hs.hdev);
@@ -209,10 +221,19 @@ static void radio_hci_smd_deregister(void)
 
 	smd_close(hs.fm_channel);
 	hs.fm_channel = 0;
+#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
+	fmsmd_ready = -1;
+#else
+done:
 	fmsmd_set = 0;
+#endif
 }
 
+#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
+int radio_hci_smd_init(void)
+#else
 static int radio_hci_smd_init(void)
+#endif
 {
 	int ret;
 
@@ -232,6 +253,7 @@ static int radio_hci_smd_init(void)
 	return ret;
 }
 
+#ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 static void radio_hci_smd_exit(void)
 {
 	if (!chan_opened) {
@@ -266,6 +288,7 @@ done:
 	mutex_unlock(&fm_smd_enable);
 	return ret;
 }
+#endif
 MODULE_DESCRIPTION("FM SMD driver");
 MODULE_AUTHOR("Ankur Nandwani <ankurn@codeaurora.org>");
 MODULE_LICENSE("GPL v2");
